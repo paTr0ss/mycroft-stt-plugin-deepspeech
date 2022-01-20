24803b0201f2ee855ef805e064d14dc18e95440e
paTr0ss <developing@rossbach.biz>
2022-01-20 22:12:41 +0100

    update to deepspeech release 0.9.3, add boost option for individual words

--- mycroft_stt_plugin_deepspeech/__init__.py
+++ mycroft_stt_plugin_deepspeech/__init__.py
@@ -1,52 +1,80 @@
 import deepspeech
 import numpy
-import time
 from mycroft.stt import STT
 from mycroft.util.log import LOG
 
 
 class DeepspeechSTTPlugin(STT):
+    """
+        Mozilla Deepspeech Speech to Text
+        Enables Mozilla Deepspeech by directly using a local installation via python binding. 
+        Deepspeech must be installed. It must be configured locally in your config file and
+        should match the following example:
+
+        "stt": {
+            "module": "deepspeech_stt_plug",
+            "deepspeech_stt_plug": {
+                "model": "/home/pi/mycroft-core/deepspeech/kenlm_de.tflite",
+                "scorer": "/home/pi/mycroft-core/deepspeech/kenlm_de.scorer",
+                "beam_width": 500,
+                "lm_alpha": 0.931289039105002,
+                "lm_beta": 1.1834137581510284
+                "booster_per_words": {
+                    "emma": 23.8,
+                    "starte": 1.9
+                }
+            }
+        }
+
+        Only mandatory attribut is "model". Ensure, configured model fits to configured language.
+
+        Default value for "beam_with" is 1024.
+
+        "booster_per_words" is a list of words and boost values, 
+        see https://deepspeech.readthedocs.io/en/latest/Python-API.html#native_client.python.Model.addHotWord
+        Thereby, you can boost words from *.intent or *.voc that are otherwise not accurately recognized.
+        
+    """
+
     def __init__(self):
         super().__init__()
 
-        start_time = time.perf_counter()
-        LOG.info("Loading DeepSpeech model...")
+        model = self.config.get('model', None)
+        if not model:
+            raise Exception('model not configured!')
 
-        model = self.config['model']
-        alphabet = self.config['alphabet']
-        num_context = self.config.get('num_context', 9)
-        beam_width = self.config.get('beam_width', 512)
-        num_features = self.config.get('num_features', 26)
-        lm = self.config.get('lm')
-        trie = self.config.get('trie')
+        LOG.info(f"Loading DeepSpeech model {model}.")
+        self.ds = deepspeech.Model(model)
 
-        self.model = deepspeech.Model(model, num_features, num_context,
-                                      alphabet, beam_width)
+        beam_width = self.config.get('beam_width', 1024)
+        LOG.info(f"Set beam width {beam_width}.")
+        self.ds.setBeamWidth(beam_width)
 
-        if lm is not None and trie is not None:
-            lm_weight = self.config.get('lm_weight', 1.5)
-            vwcw = self.config.get('valid_word_count_weight', 2.25)
+        scorer = self.config.get('scorer', None)
+        if scorer:
+            LOG.info(f"Enabling scorer {scorer}.")
+            self.ds.enableExternalScorer(scorer)
 
-            self.model.enableDecoderWithLM(alphabet, lm, trie, lm_weight, vwcw)
+            lm_alpha = self.config.get('lm_alpha', None)
+            lm_beta = self.config.get('lm_beta', None)
+            if lm_alpha and lm_beta:
+                LOG.info(f"Set alpha {lm_alpha} and beta {lm_beta}.")
+                self.ds.setScorerAlphaBeta(lm_alpha, lm_beta)
 
-        LOG.info("Loaded DeepSpeech model in %0.3fs" % (time.perf_counter() -
-                                                        start_time))
-        self.stream_ctx = None
-        self.can_stream = True
+        boosters_per_word = self.config.get('booster_per_words', None)
+        if boosters_per_word:
+            for w in boosters_per_word:
+                LOG.info(f"Add boost {boosters_per_word[w]} for {w}.")
+                self.ds.addHotWord(w, boosters_per_word[w])
+
 
     def execute(self, audio, language=None):
-        text = self.model.finishStream(self.stream_ctx)
-        self.stream_ctx = None
+        if language and language != self.lang:
+            LOG.warning(f"Given language {language} ignored, using {self.lang}")
+
+        # LOG.info(f"sample rate: {self.ds.sampleRate:6.2f} == {audio.sample_width:6.2f}")
+        LOG.info("Perform stt audio ...")
+        text = self.ds.stt(numpy.frombuffer(audio.get_raw_data(), numpy.int16))
+
         LOG.info(text)
         return text
-
-    def stream_start(self, language=None):
-        self.stream_ctx = self.model.setupStream()
-
-    def stream_data(self, data):
-        self.model.feedAudioContent(
-            self.stream_ctx, numpy.frombuffer(data, numpy.int16))
-
-    def stream_stop(self):
-        LOG.info("stop")
-        self.stream_ctx = None
