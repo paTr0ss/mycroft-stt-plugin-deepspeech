c3bd6758f66db5624000133f38ccdeb0e102ec51
paTr0ss <developing@rossbach.biz>
2022-01-23 16:23:48 +0100

    add support of Coqui STT 1.1.0

--- mycroft_stt_plugin_deepspeech/__init__.py
+++ mycroft_stt_plugin_deepspeech/__init__.py
@@ -1,4 +1,9 @@
-import deepspeech
+
+try:
+    import stt as stt_gen
+except ImportError:
+    import deepspeech as stt_gen
+
 import numpy
 from mycroft.stt import STT
 from mycroft.util.log import LOG
@@ -6,9 +11,9 @@ from mycroft.util.log import LOG
 
 class DeepspeechSTTPlugin(STT):
     """
-        Mozilla Deepspeech Speech to Text
-        Enables Mozilla Deepspeech by directly using a local installation via python binding. 
-        Deepspeech must be installed. It must be configured locally in your config file and
+        Mozilla Deepspeech Speech to Text (STT) or it's successor Coqui STT
+        Enables Coqui STT or Mozilla Deepspeech STT  by directly using a local installation via python binding. 
+        Either Coqui STT or Deepspeech must be installed. It must be configured locally in your config file and
         should match the following example:
 
         "stt": {
@@ -43,29 +48,29 @@ class DeepspeechSTTPlugin(STT):
         if not model:
             raise Exception('model not configured!')
 
-        LOG.info(f"Loading DeepSpeech model {model}.")
-        self.ds = deepspeech.Model(model)
+        LOG.info(f"Loading STT model {model}.")
+        self.stt_model = stt_gen.Model(model)
 
         beam_width = self.config.get('beam_width', 1024)
         LOG.info(f"Set beam width {beam_width}.")
-        self.ds.setBeamWidth(beam_width)
+        self.stt_model.setBeamWidth(beam_width)
 
         scorer = self.config.get('scorer', None)
         if scorer:
             LOG.info(f"Enabling scorer {scorer}.")
-            self.ds.enableExternalScorer(scorer)
+            self.stt_model.enableExternalScorer(scorer)
 
             lm_alpha = self.config.get('lm_alpha', None)
             lm_beta = self.config.get('lm_beta', None)
             if lm_alpha and lm_beta:
                 LOG.info(f"Set alpha {lm_alpha} and beta {lm_beta}.")
-                self.ds.setScorerAlphaBeta(lm_alpha, lm_beta)
+                self.stt_model.setScorerAlphaBeta(lm_alpha, lm_beta)
 
         boosters_per_word = self.config.get('booster_per_words', None)
         if boosters_per_word:
             for w in boosters_per_word:
                 LOG.info(f"Add boost {boosters_per_word[w]} for {w}.")
-                self.ds.addHotWord(w, boosters_per_word[w])
+                self.stt_model.addHotWord(w, boosters_per_word[w])
 
 
     def execute(self, audio, language=None):
@@ -74,7 +79,7 @@ class DeepspeechSTTPlugin(STT):
 
         # LOG.info(f"sample rate: {self.ds.sampleRate:6.2f} == {audio.sample_width:6.2f}")
         LOG.info("Perform stt audio ...")
-        text = self.ds.stt(numpy.frombuffer(audio.get_raw_data(), numpy.int16))
+        text = self.stt_model.stt(numpy.frombuffer(audio.get_raw_data(), numpy.int16))
 
         LOG.info(text)
         return text
